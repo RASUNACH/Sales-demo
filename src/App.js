7ced3c694f71b70b01b746dc86953a3e19f9abca
Rasunach33 <104362261+Rasunach33@users.noreply.github.com>
2022-09-13 19:42:38 +0530

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Carousel_component from './Components/Carousel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function App() {
+  <Carousel_component/>
+}
+
+export default App;
